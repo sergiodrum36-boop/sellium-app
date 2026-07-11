c2708a9176e1bcc57c0b658ffd977b30c0fb777e
Sergio <sergiodrum36@gmail.com>
2026-07-11 21:57:48 +0200

    Actualizacion automatica

--- src/Layout.js
+++ src/Layout.js
@@ -461,7 +461,7 @@ function Layout({
               igual que el resto de textos de este pie (email, "Viendo
               como"...). */}
           {!colapsadoVisual && (
-            <div className="pt-3 mt-2 border-t border-slate-200 dark:border-white/10">
+            <div className="pt-3 mt-2">
               <span className="block text-center text-[10px] font-semibold text-slate-400 dark:text-slate-500 uppercase tracking-wide mb-1.5">Powered by</span>
               <img src={logoSellium} alt="Sellium" className="w-full h-auto rounded-md" />
             </div>
